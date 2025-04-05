dfefb7b36caceda159cd7939f5c9572dc8cbc155
maluuux <xxaxaxa747@gmail.com>
2025-04-06 01:41:31 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "กำลังอัปเดต CCminer..."
-wget -O ccminer https://raw.githubusercontent.com/username/my-ccminer/main/ccminer
-wget -O config.json https://raw.githubusercontent.com/username/my-ccminer/main/config.json
-wget -O start.sh https://raw.githubusercontent.com/username/my-ccminer/main/start.sh
+wget -O  https://raw.githubusercontent.com/maluuux/termux-miner/main/ccminer
+wget -O https://raw.githubusercontent.com/maluuux/termux-miner/main/config.json
+wget -O https://raw.githubusercontent.com/maluuux/termux-miner/main/start.sh 
 chmod +x ccminer start.sh
 echo "อัปเดตเสร็จสมบูรณ์!"
