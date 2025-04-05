4934e15bce974b2a744343822217aec749957f4b
maluuux <xxaxaxa747@gmail.com>
2025-04-06 00:32:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-
-# โหลดการตั้งค่า
-CONFIG=$(cat config.json)
-POOL=$(echo $CONFIG | jq -r '.pool')
-WALLET=$(echo $CONFIG | jq -r '.wallet')
-WORKER=$(echo $CONFIG | jq -r '.worker')
-THREADS=$(echo $CONFIG | jq -r '.threads')
-
-# เริ่มขุด
-./ccminer -a verus -o $POOL -u $WALLET.$WORKER -p x -t $THREADS
+#!/bin/sh
+~/ccminer/ccminer -c ~/ccminer/config.json
