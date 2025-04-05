dfc6b8c06c807998aa2cbb21e23f31c7c675e78c
maluuux <xxaxaxa747@gmail.com>
2025-04-06 02:33:42 +0700

    Delete update.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "กำลังอัปเดต CCminer..."
-wget -O  https://raw.githubusercontent.com/maluuux/termux-miner/main/ccminer
-wget -O https://raw.githubusercontent.com/maluuux/termux-miner/main/config.json
-wget -O https://raw.githubusercontent.com/maluuux/termux-miner/main/start.sh 
-chmod +x ccminer start.sh
-echo "อัปเดตเสร็จสมบูรณ์!"
