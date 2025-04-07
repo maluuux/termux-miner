e0886ece273739b3b877226c62c89e2f4ccb9b71
maluuux <xxaxaxa747@gmail.com>
2025-04-07 16:00:34 +0700

    Update install_termux.sh

--- install_termux.sh
+++ install_termux.sh
@@ -3,8 +3,11 @@ pkg update -y && pkg upgrade -y
 pkg install -y python git libuv openssl hwloc
 
 echo "📥 ดาวน์โหลด ccminer"
-git clone https://github.com/maluuux/termux-miner/main/ccminer
-cd ccminer
+mkdir ccminer && cd ccminer
+wget  https://raw.githubusercontent.com/maluuux/termux-miner/main/ccminer
+wget  https://raw.githubusercontent.com/maluuux/termux-miner/main/config.json
+wget  https://raw.githubusercontent.com/maluuux/termux-miner/custom_miner.py
+wget  https://raw.githubusercontent.com/maluuux/termux-miner/main/run_miner.sh
 
 chmod +x ccminer custom_miner.py
 
