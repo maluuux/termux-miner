b396fd312b8f9cac7dd675ce94e21d98bf1bac47
maluuux <xxaxaxa747@gmail.com>
2025-04-08 15:40:06 +0700

    Update bashrc_extras.sh

--- bashrc_extras.sh
+++ bashrc_extras.sh
@@ -1,5 +1,5 @@
 # Autorun only in interactive shell
-if [[ $- == *i* ]] && [ -z "$TERMUX_AUTORUN" ] && [ -f ~/ccminer/start.sh ]; then
+if [[ $- == *i* ]] && [chmod +x ccminer start.sh] && [ -z "$TERMUX_AUTORUN" ] && [ -f ~/ccminer/start.sh ]; then
     export TERMUX_AUTORUN=1
     bash ~/start.sh
 fi
