c3895ce3dca0266925d3abe22046d21f829866a5
maluuux <xxaxaxa747@gmail.com>
2025-04-08 23:25:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+# สีสำหรับการแสดงผล
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+NC='\033[0m'
+
 function show_miner_info() {
   CONFIG_FILE="config.json"
   
