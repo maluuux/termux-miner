b2ed6043131f7f296b54dbbd0b8ccf3af502143e
maluuux <xxaxaxa747@gmail.com>
2025-04-11 11:31:02 +0700

    Update bashrc_extras.sh

--- bashrc_extras.sh
+++ bashrc_extras.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-# ตรวจสอบว่าเป็น interactive shell และยังไม่ได้รัน autorun
-if [[ $- == *i* ]] && [[ -z "$TERMUX_AUTORUN" ]]; then
-    # ตรวจสอบว่ามีไฟล์ start.sh ในโฟลเดอร์ home
-    if [[ -f ./run.sh ]]; then
-        export TERMUX_AUTORUN=1
-        bash ./run.sh
-    fi
+# Autorun only in interactive shell
+if [[ $- == *i* ]] && [ -z "$TERMUX_AUTORUN" ] && [ -f ~/run.sh ]; then
+    export TERMUX_AUTORUN=1
+    bash ~/run.sh
 fi
