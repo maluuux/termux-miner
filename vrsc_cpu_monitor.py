1122fe67bdbf28e94b063832f0fed0812a49e31c
maluuux <xxaxaxa747@gmail.com>
2025-04-12 21:13:52 +0700

    Update vrsc_cpu_monitor.py

--- vrsc_cpu_monitor.py
+++ vrsc_cpu_monitor.py
@@ -245,9 +245,8 @@ class VrscCpuMinerMonitor:
             ratio = (accepted / total * 100) if total > 0 else 100
 
             ratio_color = 'green' if ratio > 95 else 'yellow' if ratio > 80 else 'red'
-            print(f"{COLORS['orange_bg']}{COLORS['black_text']}Shares {COLORS['reset']} =  
-                    {COLORS[ratio_color]}{ratio:.1f}%{COLORS['reset']}")
-            print(f"            {COLORS['green']}[yes!!🚀 {accepted} ]{COLORS['reset']}")
+            print(f"{COLORS['orange_bg']}{COLORS['black_text']}Shares {COLORS['reset']} = {COLORS[ratio_color]}{ratio:.1f}%{COLORS['reset']}"),
+            print(f"            {COLORS['green']}[yes!!🚀 {accepted} ]{COLORS['reset']}"),
             print(f"            {COLORS['red'  ]}[Boo!!💢 {rejected} ]{COLORS['reset']}")
         
         
