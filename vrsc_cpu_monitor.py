29a891b879bc6d90a9d4fe3e7de74e68fb5ae5f5
maluuux <xxaxaxa747@gmail.com>
2025-04-12 17:56:02 +0700

    Update vrsc_cpu_monitor.py

--- vrsc_cpu_monitor.py
+++ vrsc_cpu_monitor.py
@@ -235,7 +235,7 @@ class VrscCpuMinerMonitor:
             diff_color = 'green' if current_diff < 100000 else 'brown' if current_diff < 300000 else 'yellow'
             print(f"  {COLORS['yellow_bg']}{COLORS['black_text']}Difficulty {COLORS['reset']}: {COLORS[diff_color]}{current_diff:.2f}{COLORS['reset']}")
             if 'difficulty' not in miner_data:
-                print(f"  {COLORS['yellow']       ʕ⁀ᴥ⁀ʔ{COLORS['reset']}")
+                print(f"  {COLORS['yellow']}       ʕ⁀ᴥ⁀ʔ{COLORS['reset']}")
         else:
             print(f"  {COLORS['yellow_bg']}{COLORS['black_text']}Difficulty {COLORS['reset']}: {COLORS['yellow']}ไม่พบข้อมูล{COLORS['reset']}")
         
