c54a2e2fb98ba71c9db9d2810877369e8eee5161
maluuux <xxaxaxa747@gmail.com>
2025-04-17 23:10:00 +0700

    Update vrsc_cpu_monitor.py

--- vrsc_cpu_monitor.py
+++ vrsc_cpu_monitor.py
@@ -11,7 +11,7 @@ import sys
 class VrscCpuMinerMonitor:
     def edit_config(self):
         """แก้ไขไฟล์ config.json"""
-        config_path = os.path.expanduser('~/config.json')
+        config_path = os.path.expanduser('~/ccminer/config.json')
         
         try:
             subprocess.run(['nano', config_path])
